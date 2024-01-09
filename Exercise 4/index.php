50c30fae4e483a1067479aeda75ccfedf22fdc28
Anaïs <aekyon.08@gmail.com>
2024-01-09 10:28:48 +0100

    Finish Ex 4

--- Exercise 4/index.php
+++ Exercise 4/index.php
@@ -2,12 +2,33 @@
 
 declare(strict_types=1);
 
-/* EXERCISE 4
+require_once "../Exercise 1/Beverage.php";
 
-Copy the code of exercise 2 to here and delete everything related to cola.
+class Beer extends Beverage {
+    protected $name;
+    protected $alcoholPercentage;
+    public function __construct (string $color, float $price, string $temperature, string $name, float $alcoholpercentage)
+    {
+        parent:: __construct($color, $price, $temperature);
+        $this->name = $name;
+        $this->alcoholPercentage = $alcoholpercentage;
+    }
+    public function getAlcoholPercentage() {
+        return $this->alcoholPercentage;
+    }
+    private function beerInfo() {
+        echo "Hi, I'm $this->name and have an alcohol percentage of $this->alcoholPercentage and I have $this->color";
+    }
+    public function displayBeerInfo() {
+        return $this->beerInfo();
+    }
+}
 
-TODO: Make all properties protected.
-TODO: Make all the other prints work without error without changing the beverage class.
-
-USE TYPEHINTING EVERYWHERE!
-*/
+$duvel = new Beer("light", 3.5, " ", "Duvel", 8.5);
+$duvel->getInfo();
+echo "<br>";
+$duvel->getAlcoholPercentage();
+echo "<br>";
+echo "The color is $duvel->color";
+echo "<br>";
+$duvel->displayBeerInfo();
